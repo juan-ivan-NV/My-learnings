6279f4ab302e8bea50b0fb9aed5b6d492f6da82f
juan-ivan-NV <coldcram14@hotmail.com>
2020-12-09 23:13:15 -0600

    django managing users

--- Building_Web_Applications_in_Django/project2/salesmanager/admin.py
+++ Building_Web_Applications_in_Django/project2/salesmanager/admin.py
@@ -8,6 +8,14 @@ class ClientAdmin(admin.ModelAdmin):
     list_display=("name", "direction", "phone")
     search_fields=("name", "phone")
 
+class ItemsAdmin(admin.ModelAdmin):
+    list_filter=("section",)
+
+class SalesAdmin(admin.ModelAdmin):
+    list_display=("number","date")
+    list_filter=("date",)
+    date_hierarchy="date"
+
 admin.site.register(clients, ClientAdmin)
-admin.site.register(items)
-admin.site.register(sales)
+admin.site.register(items, ItemsAdmin)
+admin.site.register(sales, SalesAdmin)
