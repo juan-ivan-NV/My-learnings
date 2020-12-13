5eebafcc8bd33edff32a8dc379da0212f7a5b539
juan-ivan-NV <coldcram14@hotmail.com>
2020-12-12 22:25:09 -0600

    django showing found items

--- Building_Web_Applications_in_Django/project2/salesmanager/views.py
+++ Building_Web_Applications_in_Django/project2/salesmanager/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from salesmanager.models import items
 
 # Create your views here.
 
@@ -9,6 +10,17 @@ def searching_products(request):
 
 def search(request):
 
-    msg = "Item searched: %r"% request.GET["prd"]
+    if request.GET["prd"]:
+
+        #msg = "Item searched: %r"% request.GET["prd"]
+        it_1 = request.GET["prd"]
+
+        it_all = items.objects.filter(name__icontains = it_1) 
+
+        return render(request, "searchresults.html", {"items": it_all, "query":it_1})
+    
+    else:
+        
+        msg = "Please type valid characters"
 
     return HttpResponse(msg)
