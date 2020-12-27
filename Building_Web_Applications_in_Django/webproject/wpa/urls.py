73e40cea1ad9a0c47da1c66c6969d6dfa5fa319b
juan-ivan-NV <coldcram14@hotmail.com>
2020-12-26 23:52:00 -0600

    django starting with blog categories

--- Building_Web_Applications_in_Django/webproject/wpa/urls.py
+++ Building_Web_Applications_in_Django/webproject/wpa/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',views.home, name="Homepage"),
     path('store',views.store, name="Store"),
-    path('blog',views.blog, name="Blog"),
     path('contact',views.contact, name="Contact"),
 ]
 
