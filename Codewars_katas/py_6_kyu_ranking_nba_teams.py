94264c8b4ccffc718cde5134d423faa32aeeb038
juan-ivan-NV <coldcram14@hotmail.com>
2021-12-18 21:52:07 -0600

    to fix regex for numbers

--- Codewars_katas/py_6_kyu_ranking_nba_teams.py
+++ Codewars_katas/py_6_kyu_ranking_nba_teams.py
@@ -2,9 +2,12 @@ import re
 
 def nba_cup(result_sheet, to_find):
     games = result_sheet.split(',')
-    
     match_list = [re.split(r'(?<=\d) (?=\w)', i) for i in games]
-    
-    
-    
+    print(match_list)
+    separate = lambda x: {re.sub('(?<=s )\w+', '', x).rstrip():int(re.findall('(?<=s )\w+', x)[0])}
+    #match_dict = [separate(i[0]) for i in match_list]
+    #(?<=s )\w+
+    #print(match_dict)
+    #team_1 ={re.sub('(?<=s )\w+', '', match_list[1][1]).rstrip():int(re.findall('(?<=s )\w+', match_list[1][0])[0])}
+    #print(team_1)
     return "testing"
