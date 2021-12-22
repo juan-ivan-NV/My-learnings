05bc8dfbfcb33bf45e25373770cc887af20499f4
juan-ivan-NV <coldcram14@hotmail.com>
2021-12-21 23:19:37 -0600

    testing a without re

--- Codewars_katas/py_6_kyu_ranking_nba_teams.py
+++ Codewars_katas/py_6_kyu_ranking_nba_teams.py
@@ -3,11 +3,15 @@ import re
 def nba_cup(result_sheet, to_find):
     games = result_sheet.split(',')
     match_list = [re.split(r'(?<=\d) (?=\w)', i) for i in games]
-    #print(match_list)
-    separate = lambda x: {re.sub('(?<=s) \d+', '', x).rstrip():re.findall('(?<=s )\d+', x)}
-    match_dict = [[separate(i[0]), separate(i[1])] for i in match_list]
-    #(?<=s )\w+
-    print(match_dict)
-    #team_1 ={re.sub('(?<=s )\w+', '', match_list[1][1]).rstrip():int(re.findall('(?<=s )\w+', match_list[1][0])[0])}
-    #print(team_1)
+    print(match_list)
+    
+    match_dicts = []
+    
+    for match in match_list:
+        matchd = []
+        for team in match:
+            
+            for word in match.strip().split(' '):
+                if word.isdigit():
+                    
     return "testing"
