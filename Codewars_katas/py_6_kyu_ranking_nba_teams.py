5f7cef4e2ee6b5d4e549679d5f625bd6b4041c8c
juan-ivan-NV <coldcram14@hotmail.com>
2021-12-22 22:56:17 -0600

    8 of 10 test cases passed

--- Codewars_katas/py_6_kyu_ranking_nba_teams.py
+++ Codewars_katas/py_6_kyu_ranking_nba_teams.py
@@ -1,17 +1,22 @@
 import re
 
 def nba_cup(result_sheet, to_find):
-    games = result_sheet.split(',')
-    match_list = [re.split(r'(?<=\d) (?=\w)', i) for i in games]
-    print(match_list)
     
-    match_dicts = []
+    win, draw, loss, points, conceded = 0,0,0,0,0
     
-    for match in match_list:
-        matchd = []
-        for team in match:
+    for t1, s1, t2, s2 in re.findall(r'(.+?) (\b[\d.]+\b) (.+?) (\b[\d.]+\b)(?:,|$)', result_sheet):
+        
+        if to_find == t1 or to_find == t2:
+            if to_find == t1: ps,cs = int(s1),int(s2)
+            else: ps,cs = int(s2),int(s1)
             
-            for word in match.strip().split(' '):
-                if word.isdigit():
-                    
-    return "testing"
+            points += ps
+            conceded += cs
+            
+            if s1 == s2: draw += 1
+            elif ps < cs: loss += 1
+            else: win += 1
+    
+    ranking = win * 3 + draw
+    
+    return f"{to_find}:W={win};D={draw};L={loss};Scored={points};Conceded={conceded};Points={ranking}"
