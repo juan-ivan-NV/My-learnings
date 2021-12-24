5b44c31094957203e7aa5e5c67d4066550e4691e
juan-ivan-NV <coldcram14@hotmail.com>
2021-12-23 22:09:22 -0600

    ranking nba teams finnished

--- Codewars_katas/py_6_kyu_ranking_nba_teams.py
+++ Codewars_katas/py_6_kyu_ranking_nba_teams.py
@@ -2,9 +2,13 @@ import re
 
 def nba_cup(result_sheet, to_find):
     
+    if not to_find: return ''
+    
     win, draw, loss, points, conceded = 0,0,0,0,0
     
     for t1, s1, t2, s2 in re.findall(r'(.+?) (\b[\d.]+\b) (.+?) (\b[\d.]+\b)(?:,|$)', result_sheet):
+        
+        if '.' in s1 or '.' in s2: return f"Error(float number):{t1} {s1} {t2} {s2}"
         
         if to_find == t1 or to_find == t2:
             if to_find == t1: ps,cs = int(s1),int(s2)
@@ -18,5 +22,6 @@ def nba_cup(result_sheet, to_find):
             else: win += 1
     
     ranking = win * 3 + draw
+    if not points and not conceded: return f"{to_find}:This team didn't play!"
     
     return f"{to_find}:W={win};D={draw};L={loss};Scored={points};Conceded={conceded};Points={ranking}"
